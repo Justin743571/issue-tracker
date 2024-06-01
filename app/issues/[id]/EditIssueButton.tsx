7fbc86946434c3d41eb08a43b8d5bdabe2ab1fa1
王旭 <wxu743571@gmail.com>
2024-06-01 13:50:26 +0800

    改善用户体验

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -1,13 +1,15 @@
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button>
-      <Pencil2Icon />
-      <Link href={`/issues/edit/${issueId}`}>Edit Issue</Link>
-    </Button>
+    <Link href={`/issues/edit/${issueId}`} legacyBehavior>
+      <Button>
+        <Pencil2Icon />
+        Edit Issue
+      </Button>
+    </Link>
   );
 };
 
