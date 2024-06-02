eb2ed12195c1b8126b735fb11c06a97a6cfcd933
王旭 <wxu743571@gmail.com>
2024-06-02 21:06:33 +0800

    实施分页

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+
+export default function Home({searchParams}:{searchParams: { page: string }}) {
   return (
-    <div><Pagination itemCount={100} pageSize={10} currentPage={10}/></div>
-  )
+    <div>
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+    </div>
+  );
 }
