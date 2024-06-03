aeb122a5c812271eaeb7aa44388a469266ccec6e
王旭 <wxu743571@gmail.com>
2024-06-03 12:40:01 +0800

    添加.env.example文件

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Select } from "@radix-ui/themes";
+import { Status } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const IssueStatusFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const statuses: {
+    label: string;
+    value?: Status;
+  }[] = [
+    { label: "All" },
+    { label: "Open", value: "OPEN" },
+    { label: "In Progress", value: "IN_PROGRESS" },
+    { label: "Closed", value: "CLOSED" },
+  ];
+
+  return (
+    <Select.Root
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy") || "");
+        const query = params.size ? "?" + params.toString() : "";
+        router.push(`/issues/list${query}`);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.label} value={status.value || ""}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
