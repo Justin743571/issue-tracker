45157a9017faffff99f97f9ce2144fbb3160d6ae
王旭 <wxu743571@gmail.com>
2024-06-03 12:24:10 +0800

    使用React Cache 优化性能

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -1,37 +1,34 @@
-import React from 'react'
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-import dynamic from 'next/dynamic';
-import IssueFormSkeleton from './loading';
+import React, { cache } from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
 
-const IssueForm = dynamic(
-  () => import('@/app/issues/_components/IssueForm'),
-  { 
-    ssr: false, 
-    loading: () => <IssueFormSkeleton />
-  }
-)
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  })
+);
+
 const EditIssuePage = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id)}
-  });
+  const issue = await fetchIssue(parseInt(params.id));
 
   if (!issue) notFound();
 
-  return (
-    <IssueForm issue={issue} />
-  )
-}
+  return <IssueForm issue={issue} />;
+};
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
 
   return {
     title: "Editing " + issue?.title,
@@ -39,4 +36,4 @@ export async function generateMetadata({ params }: Props) {
   };
 }
 
-export default EditIssuePage
+export default EditIssuePage;
